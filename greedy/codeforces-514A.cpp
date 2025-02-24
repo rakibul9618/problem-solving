563328f4615a58e49b480d389013ccbdd6aee246
Rakibul <rakibul.onnorokom@gmail.com>
2025-02-24 10:57:42 +0600

    solve the problem of codeforces 514A

--- greedy/codeforces-514A.cpp
+++ greedy/codeforces-514A.cpp
@@ -1,10 +1,35 @@
 // https://codeforces.com/contest/514/problem/A
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
+
+  string value{};
+
+  cin >> value;
+
+  char firstChar = value[0];
+
+  for (size_t i = 0; i < value.size(); i++)
+  {
+
+    int t = value[i] - '0';
+
+    if (9 - t < t)
+    {
+      value[i] = (9 - t) + '0';
+    }
+  }
+
+  if (value[0] == '0')
+  {
+    value[0] = firstChar;
+  }
+
+  cout << value << endl;
 
   return 0;
 }
