d864f3814caa36e88d3d7268b3737d0b83fae76b
Rakibul <rakibul.onnorokom@gmail.com>
2025-02-25 18:04:19 +0600

    solve the problem of 1207A

--- greedy/codeforces-1207A.cpp
+++ greedy/codeforces-1207A.cpp
@@ -1,10 +1,44 @@
 // https://codeforces.com/contest/1207/problem/A
 
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
+
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++)
+  {
+    int b, p, f, h, c, profit{0};
+    cin >> b >> p >> f >> h >> c;
+
+    int availableBurger = b / 2;
+
+    if (h > c)
+    {
+      int burgerWithPatties = min(availableBurger, p);
+      profit += (burgerWithPatties * h);
+      availableBurger -= burgerWithPatties;
+
+      int burgerWithCutlets = min(availableBurger, f);
+      profit += (burgerWithCutlets * c);
+      cout << profit << endl;
+    }
+    else
+    {
+      int burgerWithCutlets = min(availableBurger, f);
+      profit += (burgerWithCutlets * c);
+      availableBurger -= burgerWithCutlets;
+
+      int burgerWithPatties = min(availableBurger, p);
+      profit += (burgerWithPatties * h);
+
+      cout << profit << endl;
+    }
+  }
 
   return 0;
 }
